4fd5116179f99f475a01355f23e266a13c468170
Muh Suryadi Triputra Rahman <msuryaditriputra@gmail.com>
2021-10-14 12:30:45 +0800

    add Rellax and GSAP animate

--- assets/js/script.js
+++ assets/js/script.js
@@ -10,6 +10,60 @@ const showMenu = (toggleId, navId) => {
 
 showMenu("nav-toggle", "nav-menu");
 
-/* === ===*/
-/* === ===*/
+/* === Rellax.js ===*/
+let rellax = new Rellax(".parallax");
+
+/* === GSAP ANIMATION ===*/
+// NAVBAR
+gsap.from(".nav__logo", {
+  opacity: 0,
+  duration: 3,
+  delay: 0.5,
+  y: 30,
+  ease: "expo.out",
+});
+
+gsap.from(".nav__toggle", {
+  opacity: 0,
+  duration: 3,
+  delay: 0.7,
+  y: 30,
+  ease: "expo.out",
+});
+
+gsap.from(".nav__item", {
+  opacity: 0,
+  duration: 3,
+  delay: 0.7,
+  y: 35,
+  ease: "expo.out",
+  stagger: 0.2,
+});
+
+// Text
+gsap.from(".home__title", {
+  opacity: 0,
+  duration: 3,
+  delay: 1.3,
+  y: 35,
+  ease: "expo.out",
+});
+
+gsap.from(".home__subtitle", {
+  opacity: 0,
+  duration: 3,
+  delay: 1.1,
+  y: 35,
+  ease: "expo.out",
+});
+
+// scroll
+gsap.from(".home__scroll", {
+  opacity: 0,
+  duration: 3,
+  delay: 1.5,
+  y: 35,
+  ease: "expo.out",
+});
+
 /* === ===*/
